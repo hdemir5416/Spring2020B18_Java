e1b2bc92f25d78436ff7cc6991e4eb39725bc98f
hdemir5416 <hdemir5416@gmail.com>
2020-04-26 15:49:54 -0600

    changes

--- src/gittest/git2.java
+++
@@ -1,7 +0,0 @@
-package gittest;
-
-public class git2 {
-    public static void main(String[] args) {
-        System.out.println("jfjfn");
-    }
-}
--- src/gittest/gitclass.java
+++
@@ -1,8 +0,0 @@
-package gittest;
-
-public class gitclass {
-    public static void main(String[] args) {
-        System.out.println("tst");
-        System.out.println("tesr11");   
-    }
-}
